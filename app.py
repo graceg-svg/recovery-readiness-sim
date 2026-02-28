4eeeef675cb75902e47410e9c3b3187f23327ef8
graceg-svg <gracejgolen@gmail.com>
2026-02-28 16:43:35 -0500

    Remove main function and docstring from app.py

--- app.py
+++ app.py
@@ -2,14 +2,3 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-
-"""
-# Recovery Readiness Simulation App
-
-def main():
-    print("Welcome to Recovery Readiness Simulation")
-    pass
-
-if __name__ == "__main__":
-    main()
-"""
